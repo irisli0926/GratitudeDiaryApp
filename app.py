12442b26bcc79da6e932c1ff843b826acaf33ecc
michelle-mrz <michelle.ren00@gmail.com>
2023-11-19 21:10:56 -0500

    created app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import json 
+from db import db
+from flask import Flask
+from db import User
+from db import Entry
+from db import GratitudeMessage
+
+app = Flask(__name__)
+db_filename = "todo.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+# initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps({"success": True, "data": data}), code
+
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}), code
+
+# your routes here
+@app.route("/api/users/")
+def get_users():
+    users = [u.serialize() for u in User.query.all()]
+    return success_response(users)
+
