47e6052965918b22356bfbed1b1372746c0d99d1
Maggie Lam <143830769+MaggieLam2003@users.noreply.github.com>
2023-12-03 03:14:12 -0500

    created friends interface

--- HackChallenge/Journal/GratitudeJournal/GratitudeJournal/Views/Friends.swift
+++ HackChallenge/Journal/GratitudeJournal/GratitudeJournal/Views/Friends.swift
@@ -7,12 +7,26 @@
 
 import SwiftUI
 
+struct User {
+    let name: String
+    let userID: String
+    let imageName: String
+}
+
 struct Friends: View {
     @State private var tabSelected: Tab = .person
     
     init() {
         UITabBar.appearance().isHidden = true
     }
+    
+    let users: [User] = [
+        User(name: "Alice Kanning", userID: "1234", imageName: "user1"),
+        User(name: "Bob Junior", userID: "5678", imageName: "user2"),
+        User(name: "Karen Smith", userID: "9008", imageName: "user3"),
+        User(name: "Babpy Wish", userID: "6738", imageName: "user4"),
+        User(name: "Shinnah Zenh", userID: "7222", imageName: "user5")
+    ]
     
     var body: some View {
         ZStack {
@@ -40,7 +54,7 @@ struct Friends: View {
                             
                             
                         }
-                        Text("you have 346 friends")
+                        Text("you have \(users.count) friends")
                             .font(Font.custom("KumbhSans-Regular", size: 16))
                             .fontWeight(.medium)
                             .foregroundColor(.white)
@@ -50,17 +64,50 @@ struct Friends: View {
                 }
                 .padding(.bottom, 24)
                 
-                VStack(spacing: 16) {
-//                    White space, insert other stuff here
+//                White part
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 16) {
+                        
 
+                        ForEach(users, id: \.userID) { user in
+                            HStack(alignment: .center) {
+                                Image(user.imageName)
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipShape(Circle())
+                                    .padding(8)
+
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(user.name)
+                                        .font(.headline)
+
+                                    Text("ID: \(user.userID)")
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                }
+                                .padding(.vertical, 12)
+
+                                Spacer()
+                            }
+                            .padding(12)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color(red: 0.75, green: 0.9, blue: 0.93))
+                            .cornerRadius(10)
+                            .padding(.vertical, 4)
+                        }
+                    }
+                    .padding(24)
+                    .background(Color.white)
+                    .cornerRadius(20)
                 }
-                .padding(24)
-                .padding(.bottom, 300)
-                .background(Color.white)
-                .cornerRadius(20)
                 .frame(maxHeight: .infinity, alignment: .top)
-
             }
+
+            
+            
+            
 
         }
         .edgesIgnoringSafeArea(.bottom)
